13ce063b6d6e64bfb0d42164b0550501d0536fc7
Issei Fujimoto <otomijuf.004@gmail.com>
2018-05-08 21:44:43 +0900

    import edit

--- word2vec_trainer.py
+++ word2vec_trainer.py
@@ -1,17 +1,18 @@
-from lib import Const
-from lib import WordVec as wv
 
-fname = Const.Const().word2vec_train_file
+from lib.WordVec import MyWord2Vec
+from lib.Const import Const
 
-#w2v = wv.MyWord2Vec().train(fname, "save")
-model = wv.MyWord2Vec().load_model()
+fname = Const.word2vec_train_file
+
+#w2v = MyWord2Vec.train(fname, "save")
+model = MyWord2Vec.load_model()
 
 print("corpus: ", model.corpus_count)
 voc = model.wv.vocab.keys()
 print("vocab: ", len(voc))
 print(voc)
 
-# vec = wv.MyWord2Vec().str_to_vector(model, "引か")
+# vec = MyWord2Vec.str_to_vector(model, "引か")
 # print(vec)
 # import pylab as plt
 # plt.plot(vec)
