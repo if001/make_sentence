5a40f7758a47e918e2f991f90c449b778ba4edd3
IsseiFUJIMOTO <i.fujimoto@accelia.net>
2018-05-09 13:12:52 +0900

    train test　分岐

--- word2vec_trainer.py
+++ word2vec_trainer.py
@@ -2,18 +2,26 @@
 from lib.WordVec import MyWord2Vec
 from lib.Const import Const
 
-fname = Const.word2vec_train_file
 
-#w2v = MyWord2Vec.train(fname, "save")
-model = MyWord2Vec.load_model()
+def train():
+    fname = Const.word2vec_train_file
+    w2v = MyWord2Vec.train(fname, "save")
 
-print("corpus: ", model.corpus_count)
-voc = model.wv.vocab.keys()
-print("vocab: ", len(voc))
-print(voc)
 
-# vec = MyWord2Vec.str_to_vector(model, "引か")
-# print(vec)
-# import pylab as plt
-# plt.plot(vec)
-# plt.show()
+def test():
+    model = MyWord2Vec.load_model()
+
+    # print("corpus: ", model.corpus_count)
+    # voc = model.wv.vocab.keys()
+    # print("vocab: ", len(voc))
+    # print(voc)
+
+    # vec = MyWord2Vec.str_to_vector(model, "引か")
+    # print(vec)
+    # import pylab as plt
+    # plt.plot(vec)
+    # plt.show()
+
+
+train()
+test()
